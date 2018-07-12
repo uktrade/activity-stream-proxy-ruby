84171f403d6ce5fc4cab30915da0c538ba841043
Michal Charemza <michal@charemza.name>
2018-07-12 14:34:05 +0100

    (feat) Return WWW-Authenticate header

--- main.rb
+++ main.rb
@@ -1,12 +1,49 @@
+require 'securerandom'
 require 'sinatra'
 
 configure do
   set :port, ENV['PORT']
 end
 
+class ExpiringSet
+  def initialize(seconds)
+    @seconds = seconds
+    @expires = {}
+  end
+
+  def remove_old_keys(now)
+     @expires = @expires.select { |item, expires| expires > now }
+  end
+
+  def add(item)
+    now = Time.now.to_i
+    remove_old_keys(now)
+    item_expires = now + @seconds
+    @expires[item] = item_expires
+  end
+
+  def include?(item)
+    now = Time.now.to_i
+    self.remove_old_keys(now)
+    @set.include(item)
+  end
+end
+
+nonce_expire = 15
+$server_nonces_generated = ExpiringSet.new(nonce_expire)
+$server_nonces_used = ExpiringSet.new(nonce_expire)
+$client_nonces_used = ExpiringSet.new(nonce_expire)
+$correct_realm = 'activity-stream-proxy-ruby'
+$correct_qop = 'auth'
+
 def respond_401()
+  nonce = SecureRandom.hex(64)
+  $server_nonces_generated.add(nonce)
+  www_authenticate = "Digest realm=\"#{$correct_realm}\", qop=\"#{$correct_qop}\", algorithm=SHA-256, nonce=\"#{nonce}\""
+
   headers = {
     'Content-Type'=>'application/json',
+    'WWW-Authenticate' => www_authenticate,
   }
   [401, headers, '{"details":"Unable to authenticate"}']
 end
