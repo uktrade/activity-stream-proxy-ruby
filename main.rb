99f00eb43cbee64154893cd97f8384b4b9c688f5
Michal Charemza <michal@charemza.name>
2018-07-12 13:30:21 +0100

    (feat) X-Forwarded-For protection

--- main.rb
+++ main.rb
@@ -4,6 +4,17 @@ configure do
   set :port, ENV['PORT']
 end
 
+def respond_401()
+  [401, {'Content-Type'=>'application/json'}, '{"details":"Unable to authenticate"}']
+end
+
+authorized_ip_addresses = ENV['ACTIVITY_STREAM_IP_WHITELIST'].split(',')
+
 get '/' do
-  [200, {'Content-Type'=>'application/json'}, '{"content”:”for-pen-test"}']
+  # IP address validation
+  return respond_401 unless request.env.key?('HTTP_X_FORWARDED_FOR')
+  remote_ips = request.env['HTTP_X_FORWARDED_FOR'].split(',')
+  return response_401 unless remote_ips.length >= 2 && authorized_ip_addresses.include?(remote_ips[-2])
+
+  [200, {'Content-Type'=>'application/json'}, '{"content":"for-pen-test"}']
 end
