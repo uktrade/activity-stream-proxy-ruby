abfe101bca7e3a7505c191e8ba6cff91f91f97f5
Michal Charemza <michal@charemza.name>
2018-07-13 07:51:01 +0100

    (docs) Commenting + whitespace

--- main.rb
+++ main.rb
@@ -71,38 +71,39 @@ get '/' do
   return respond_401 unless remote_ips.length >= 2 && settings.authorized_ip_addresses.include?(remote_ips[-2].strip)
   return respond_401 unless request.env.key?('HTTP_AUTHORIZATION')
 
+  # Header structure check
   authorization_header = request.env['HTTP_AUTHORIZATION']
   parsed_header_array = authorization_header.scan(/([a-z]+)="([^"]+)"/)
   parsed_header = parsed_header_array.each_with_object({}) do |key_val, memo|
     memo[key_val[0].to_sym] = key_val[1]
   end
-
   return respond_401 unless parsed_header.key? :nonce
   return respond_401 unless parsed_header.key? :cnonce
   return respond_401 unless parsed_header.key? :username
   return respond_401 unless parsed_header.key? :response
 
+  # Nonce check + store
   server_nonce_generated = settings.server_nonces_generated.include?(parsed_header[:nonce])
   server_nonce_available = not(settings.server_nonces_used.include?(parsed_header[:nonce]))
   client_nonce_available = not(settings.client_nonces_used.include?(parsed_header[:cnonce]))
-
   settings.server_nonces_used.add(parsed_header[:nonce])
   settings.client_nonces_used.add(parsed_header[:cnonce])
   return respond_401 unless server_nonce_generated and server_nonce_available and client_nonce_available
 
+  # Username check
   return respond_401 unless secure_compare(parsed_header[:username], settings.correct_username)
+
+  # Hash check
   hmac_data_hash = Digest::SHA256.hexdigest(
       "#{request.request_method}:#{request.fullpath}")
   hmac_secret_hash = Digest::SHA256.hexdigest(
       "#{settings.correct_username}:#{settings.correct_realm}:#{settings.correct_password}")
-
   nonce_c = '00000001'  # We only allow cnonce to be used once
   hmac_value = Digest::SHA256.hexdigest(
       "#{hmac_secret_hash}:#{parsed_header[:nonce]}:" +
       "#{nonce_c}:#{parsed_header[:cnonce]}:#{settings.correct_qop}:" +
       "#{hmac_data_hash}"
     )
-
   return respond_401 unless secure_compare(parsed_header[:response], hmac_value)
 
   [200, {'Content-Type'=>'application/json'}, '{"content":"for-pen-test"}']
