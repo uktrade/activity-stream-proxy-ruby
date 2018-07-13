8367f5bbe683f8a792fed717fcfd974d60437fd9
Michal Charemza <michal@charemza.name>
2018-07-13 07:24:48 +0100

    (refactor) Avoid globals

--- main.rb
+++ main.rb
@@ -1,10 +1,6 @@
 require 'securerandom'
 require 'sinatra'
 
-configure do
-  set :port, ENV['PORT']
-end
-
 class ExpiringSet
   def initialize(seconds)
     @seconds = seconds
@@ -29,31 +25,22 @@ class ExpiringSet
   end
 end
 
-generated_nonce_expire = 15
-$server_nonces_generated = ExpiringSet.new(generated_nonce_expire)
+configure do
+  set :port, ENV['PORT']
 
-client_nonce_expire = 60
-$server_nonces_used = ExpiringSet.new(client_nonce_expire)
-$client_nonces_used = ExpiringSet.new(client_nonce_expire)
-$correct_realm = 'activity-stream-proxy-ruby'
-$correct_qop = 'auth'
+  set :authorized_ip_addresses, ENV['ACTIVITY_STREAM_IP_WHITELIST'].split(',')
+  set :correct_username, ENV['KEY_ID']
+  set :correct_password, ENV['SECRET_KEY']
 
-def respond_401()
-  nonce = SecureRandom.hex(64)
-  $server_nonces_generated.add(nonce)
-  www_authenticate = "Digest realm=\"#{$correct_realm}\", " +
-                     "qop=\"#{$correct_qop}\", algorithm=SHA-256, nonce=\"#{nonce}\""
+  generated_nonce_expire = 15
+  set :server_nonces_generated, ExpiringSet.new(generated_nonce_expire)
 
-  headers = {
-    'Content-Type'=>'application/json',
-    'WWW-Authenticate' => www_authenticate,
-  }
-  [401, headers, '{"details":"Unable to authenticate"}']
+  client_nonce_expire = 60
+  set :server_nonces_used, ExpiringSet.new(client_nonce_expire)
+  set :client_nonces_used, ExpiringSet.new(client_nonce_expire)
+  set :correct_realm, 'activity-stream-proxy-ruby'
+  set :correct_qop, 'auth'
 end
-
-$authorized_ip_addresses = ENV['ACTIVITY_STREAM_IP_WHITELIST'].split(',')
-$correct_username = ENV['KEY_ID']
-$correct_password = ENV['SECRET_KEY']
 
 def secure_compare(a, b)
   return false if a.empty? || b.empty? || a.bytesize != b.bytesize
@@ -65,11 +52,24 @@ def secure_compare(a, b)
 end
 
 get '/' do
+  def respond_401()
+    nonce = SecureRandom.hex(64)
+    settings.server_nonces_generated.add(nonce)
+    www_authenticate = "Digest realm=\"#{settings.correct_realm}\", " +
+                       "qop=\"#{settings.correct_qop}\", algorithm=SHA-256, nonce=\"#{nonce}\""
+
+    headers = {
+      'Content-Type'=>'application/json',
+      'WWW-Authenticate' => www_authenticate,
+    }
+    [401, headers, '{"details":"Unable to authenticate"}']
+  end
+
   # IP address validation
   return respond_401 unless request.env.key?('HTTP_X_FORWARDED_FOR')
 
   remote_ips = request.env['HTTP_X_FORWARDED_FOR'].split(',')
-  return respond_401 unless remote_ips.length >= 2 && $authorized_ip_addresses.include?(remote_ips[-2].strip)
+  return respond_401 unless remote_ips.length >= 2 && settings.authorized_ip_addresses.include?(remote_ips[-2].strip)
   return respond_401 unless request.env.key?('HTTP_AUTHORIZATION')
 
   authorization_header = request.env['HTTP_AUTHORIZATION']
@@ -82,27 +82,27 @@ get '/' do
   return respond_401 unless parsed_header.key? :cnonce
   return respond_401 unless parsed_header.key? :username
   return respond_401 unless parsed_header.key? :response
-  return respond_401 unless secure_compare(parsed_header[:username], $correct_username)
-  return respond_401 unless $server_nonces_generated.include?(parsed_header[:nonce])
-  return respond_401 if $server_nonces_used.include?(parsed_header[:nonce]) 
-  return respond_401 if $client_nonces_used.include?(parsed_header[:cnonce]) 
+  return respond_401 unless secure_compare(parsed_header[:username], settings.correct_username)
+  return respond_401 unless settings.server_nonces_generated.include?(parsed_header[:nonce])
+  return respond_401 if settings.server_nonces_used.include?(parsed_header[:nonce]) 
+  return respond_401 if settings.client_nonces_used.include?(parsed_header[:cnonce]) 
 
   hmac_data_hash = Digest::SHA256.hexdigest(
       "#{request.request_method}:#{request.fullpath}")
   hmac_secret_hash = Digest::SHA256.hexdigest(
-      "#{$correct_username}:#{$correct_realm}:#{$correct_password}")
+      "#{settings.correct_username}:#{settings.correct_realm}:#{settings.correct_password}")
 
   nonce_c = '00000001'  # We only allow cnonce to be used once
   hmac_value = Digest::SHA256.hexdigest(
       "#{hmac_secret_hash}:#{parsed_header[:nonce]}:" +
-      "#{nonce_c}:#{parsed_header[:cnonce]}:#{$correct_qop}:" +
+      "#{nonce_c}:#{parsed_header[:cnonce]}:#{settings.correct_qop}:" +
       "#{hmac_data_hash}"
     )
 
   return respond_401 unless secure_compare(parsed_header[:response], hmac_value)
 
-  $server_nonces_used.add(parsed_header[:nonce])
-  $client_nonces_used.add(parsed_header[:cnonce])
+  settings.server_nonces_used.add(parsed_header[:nonce])
+  settings.client_nonces_used.add(parsed_header[:cnonce])
 
   [200, {'Content-Type'=>'application/json'}, '{"content":"for-pen-test"}']
 end
